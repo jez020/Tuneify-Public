71e0bc7a7c4a17081aa1daa63f0c06ba998532e5
Jerry Zhang <hydrogen.studio.llc@gmail.com>
2024-12-11 18:39:23 -0800

    updatete

--- src/interactions/buttons/disconnectSpotify.js
+++ src/interactions/buttons/disconnectSpotify.js
@@ -20,6 +20,6 @@ export const Button = {
             .setTitle("Your Spotify Account has been removed from our database!")
             .setDescription("Although we now have no access to your Spotify's API tokens, we suggest you to manually remove \"Tune Bot\" inside your spotify connections page to complete the disconnections.")
             .setColor("Green")
-        await interaction.followUp({ embeds: [embed], components: [], ephemeral: false })
+        await interaction.channel.send({ embeds: [embed], components: [], ephemeral: false })
     }
 }; // ButtonCommand of the deleteOutput button.
